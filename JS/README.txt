ebae626b1ebf7869932abf9a28bef0ad6ce43587
Ryan Dougherty <ryand626@gmail.com>
2014-02-07 06:23:33 -0500

    Insertion Sort js
    almost completed insertion sort js program.  Having trouble getting a
    nice visualization

--- JS/README.txt
+++ JS/README.txt
@@ -3,3 +3,5 @@ Experimenting with JavaScript to try to get a feel for the language
 trying to visualize some sorting algorithms
 
 currently working on a perceivable delay between the steps of insertion sort
+
+"works" but still trying to improve
--- JS/test.js
+++ JS/test.js
@@ -1,22 +1,28 @@
 //Example JS
+instructionList = Array();
+NumArray = Array();
+
 main();
 
 function main (){
-    wait(200);
     document.write("<p>" + new Date() + "</p>");
-    list = start(20,20);
-    document.write("HI THERE");
-    setTimeout(insertionSort(getNums("Numbers")),5000);
+    list = start(10,20);
+    document.write("<h1>Original</h1>");
+    print(list);
     
-}
+    insertionSort(getNums("Numbers"),instructionList);
 
+    document.write("<h1>instructionList</h1>")
+    print(instructionList);
+    setInterval(replace,1000);
+}
 
 function start (size,max) {
     myList = Array();
     for (var i = 0; i < size; i++) {
         myList[i] = Math.floor(Math.random()*max);
     }
-    document.write("<h1>Original List</h1>");
+    document.write("<h1>insertionSort</h1>");
     document.write("<ul>");
     for (var i = 0; i < myList.length; i++) {
         document.write("<li class = Numbers>" + myList[i] + "</li>");
@@ -33,7 +39,7 @@ function print (myList) {
     document.write("</ul>");
 }
 
-function insertionSort (myList) {
+function insertionSort (myList, instructionList) {
     var temp;
     var iter;
 
@@ -41,17 +47,21 @@ function insertionSort (myList) {
         temp = myList[i];
         for(iter = i; iter > 0 && myList[iter-1] > temp; iter--){
             myList[iter] = myList[iter-1];
-            reprint("Numbers", myList);
+            
+            instructionList[instructionList.length] = iter;
+            instructionList[instructionList.length] = iter -1;
         }
         myList[iter] = temp;
-        reprint("Numbers", myList);
+        
+        //instructionList[instructionList.length] = iter;
+        //instructionList[instructionList.length] = i;
+        
     }
-    return myList;
 }
 
 function getNums (identifier) {
     myList = document.getElementsByClassName(identifier);
-    NumArray = new Array(myList.length);
+    //NumArray = new Array(myList.length);
 
     for (var i = 0; i < myList.length; i++) {
         NumArray[i] = Number(myList[i].innerHTML);
@@ -61,17 +71,23 @@ function getNums (identifier) {
 
 function reprint (identifier, NumArray){
     myList = document.getElementsByClassName(identifier);
-    wait(5);
     for (var i = 0; i < myList.length; i++) {
         myList[i].innerHTML = NumArray[i];
     }
 }
 
-function wait (ms) {
-    var start = new Date().getTime();
-    for (var i = 0; i < 1e7; i++) {
-        if ((new Date().getTime() - start) > ms){
-            break;
-        }
+function replace(){
+    if (instructionList.length == 0){
+        return;
     }
+
+    myList = document.getElementsByClassName("Numbers");
+    var indx1 = instructionList.shift();
+    var indx2 = instructionList.shift();
+
+    console.log(indx1 + " " + indx2);
+
+    myList[indx1].innerHTML = "<b>" + NumArray[indx1] + "</b>";
+    myList[indx2].innerHTML = "<u>" + NumArray[indx2] + "</u>";
+
 }
